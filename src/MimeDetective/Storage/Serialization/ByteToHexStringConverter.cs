575520a59ddbb65309de1c471c75b0f8b37b0776
Henric Jungheim <software@henric.org>
2024-11-16 17:22:01 -0700

    Handle blobs longer than 256.

--- src/MimeDetective/Storage/Serialization/ByteToHexStringConverter.cs
+++ src/MimeDetective/Storage/Serialization/ByteToHexStringConverter.cs
@@ -6,13 +6,17 @@ using System.Text.Json;
 using System.Text.Json.Serialization;
 
 namespace MimeDetective.Storage;
+
 #if NET8_0_OR_GREATER
 static internal class ByteToHexUtility {
-    public const int MaximumLength = 256;
+    public const int MaximumStackalloc = 512;
+    public const int MaximumLength = 8 * 1024;
     private static readonly SearchValues<byte> HexSearchValues = SearchValues.Create("abcdefABCDEF0123456789"u8);
 
     public static byte[] FromHex(ReadOnlySpan<byte> input) {
-        Span<char> hexChars = stackalloc char[MaximumLength];
+        if (input.Length > MaximumLength) {
+            throw new FormatException("Input too long");
+        }
 
         if (-1 != input.IndexOfAnyExcept(HexSearchValues)) {
             throw new FormatException("Invalid characters in input");
@@ -20,18 +24,30 @@ static internal class ByteToHexUtility {
 
         input = input.Trim((byte)' ');
 
-        Ascii.ToUtf16(input, hexChars, out var written);
-        if (input.Length != written) {
-            throw new FormatException("Invalid length");
-        }
+        char[]? poolBuffer = null;
+        var hexChars = input.Length <= MaximumStackalloc
+            ? stackalloc char[MaximumStackalloc]
+            : (poolBuffer = ArrayPool<char>.Shared.Rent(input.Length)).AsSpan();
 
-        return Convert.FromHexString(hexChars[..written]);
+        try {
+            Ascii.ToUtf16(input, hexChars, out var written);
+            if (input.Length != written) {
+                throw new FormatException("Invalid length");
+            }
+
+            return Convert.FromHexString(hexChars[..written]);
+        } finally {
+            if (poolBuffer is not null) {
+                ArrayPool<char>.Shared.Return(poolBuffer);
+            }
+        }
     }
 }
 #endif // NET8_0_OR_GREATER
 
 internal abstract class ByteToHexStringConverter<T> : JsonConverter<T>
     where T : IEnumerable<byte> {
+
     protected static byte[] ReadBytes(ref Utf8JsonReader reader, JsonSerializerOptions options) {
         //var xData = JsonSerializer.Deserialize<string>(ref reader, options)
         //        ?? string.Empty
@@ -40,22 +56,31 @@ internal abstract class ByteToHexStringConverter<T> : JsonConverter<T>
         //return Convert.FromHexString(xData);
 
 #if NET8_0_OR_GREATER
-                if (reader.TokenType != JsonTokenType.String) {
-                    throw new JsonException("Expected string");
-                }
+        if (reader.TokenType != JsonTokenType.String) {
+            throw new JsonException("Expected string");
+        }
 
-                const int maximumLength = 256;
-                Span<byte> buffer = stackalloc byte[maximumLength];
-                if (reader.HasValueSequence) {
-                    if (reader.ValueSequence.Length > buffer.Length) {
-                        throw new FormatException("Input too large");
-                    }
+        if (!reader.HasValueSequence)
+            return ByteToHexUtility.FromHex(reader.ValueSpan);
 
-                    reader.ValueSequence.CopyTo(buffer);
-                    return ByteToHexUtility.FromHex(buffer[..(int)reader.ValueSequence.Length]);
-                }
+        if (reader.ValueSequence.Length > ByteToHexUtility.MaximumLength) {
+            throw new JsonException("Input too long");
+        }
 
-                return ByteToHexUtility.FromHex(reader.ValueSpan);
+        byte[]? poolBuffer = null;
+        try {
+            var buffer = reader.ValueSequence.Length <= ByteToHexUtility.MaximumStackalloc
+                ? stackalloc byte[ByteToHexUtility.MaximumStackalloc]
+                : (poolBuffer = ArrayPool<byte>.Shared.Rent((int)reader.ValueSequence.Length)).AsSpan();
+
+            reader.ValueSequence.CopyTo(buffer);
+
+            return ByteToHexUtility.FromHex(buffer[..(int)reader.ValueSequence.Length]);
+        } finally {
+            if (poolBuffer is not null) {
+                ArrayPool<byte>.Shared.Return(poolBuffer);
+            }
+        }
 #else
         var Data = reader.GetString();
 
